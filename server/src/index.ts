e704be36b6083622eb1eec95779b46f1ba74adb6
unknown <mele>
2024-01-18 14:47:07 +0000

    Add Docker support for backend services

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,18 @@
 import express from 'express'
+
+import diaryRouter from './routes/diaries'
+
 const app = express()
 app.use(express.json()) // Middleware - transforms req.body into json
 
-const PORT = 3000
+const PORT = process.env.PORT || 3001
 
 app.get('/ping', (_req, res) => {
-  console.log('ping done')
+  console.log('ping done ' + new Date().toLocaleDateString())
   res.send('pong')
 }) // endpoint
+
+app.use('/api/diaries', diaryRouter)
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
---
+++ server/src/routes/diaries.ts
@@ -0,0 +1,35 @@
+import express from 'express'
+import * as diaryServices from '../services/diaryServices'
+import toNewDiaryEntry from '../utils'
+
+const router = express.Router()
+
+router.get('/', (_req, res) => {
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
+})
+
+router.get('/:id', (req, res) => {
+  const diary = diaryServices.findById(+req.params.id)
+  // const diary = diaryServices.findById(Number(req.params.id))
+  return (diary != null)
+    ? res.send(diary)
+    : res.sendStatus(404)
+  // res.send(diary?.weather)
+})
+
+router.post('/', (req, res) => {
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body)
+
+    const addedDiaryEntry = diaryServices.addDiary(newDiaryEntry)
+
+    res.json(addedDiaryEntry)
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(400).send(e.message)
+    }
+    res.status(400).send('An unexpected error occurred.')
+  }
+})
+
+export default router
