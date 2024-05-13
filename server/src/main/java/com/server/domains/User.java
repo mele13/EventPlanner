1afcfaf816fd059cd7e3b568e400c6fddff5ede6
unknown <mele>
2024-05-13 22:02:27 +0100

    Changed from nodejs to spring boot to shorten dev phase

---
+++ server/src/main/java/com/server/domains/User.java
@@ -0,0 +1,20 @@
+package com.server.domains;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class User {
+  // @NoArgsConstructor                                                
+  // public class Person {
+  // @Getter 
+  // @Setter
+  // private Long id;
+  // @Getter 
+  // @Setter
+  // private String fName;
+  // @Getter 
+  // @Setter
+  // private String lName;
+  // }
+}
